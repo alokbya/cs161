a1ece7eeb959e1f6a7debc558b1bd778f11d3650
STUDENT IN 48287 46358 <alaa.alokby1@syccuxas01.pcc.edu>
2018-10-30 18:04:12 -0700

    drafted an outline for this weeks program, which is to calculate averages for gas prices given a text file filled with data. Started the .cpp file with 'hello, world' to verify everything is good-to-go.

---
+++ GasPrices.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(){
+    
+
+
+    // Introduction
+
+    cout << "hello, world!" << endl;
+
+    return 0;
+}
