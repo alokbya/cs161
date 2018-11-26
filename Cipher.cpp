3160ba5b10207fd08205b19d345049384bcb6270
STUDENT IN 48287 46358 <alaa.alokby1@syccuxas01.pcc.edu>
2018-11-25 23:20:54 -0800

    Cleaned up code

--- Cipher.cpp
+++ Cipher.cpp
@@ -1,4 +1,5 @@
 // Cipher.cpp
+
 // This program largely revolves around an array, alphabetArray[], which has 50 elements initialized
 // to zero at the beginning of the program. Each element corresponds to an ASCII value of a character
 // in the alphabet. Elements 0-24 are reserved for UPPERCASE letters A-Z. Elements 25-49 are
@@ -8,33 +9,42 @@
 // the element with the highest value will correspond to the letter 'e', the function will be able to make
 // an appropriate shift based on which element has the highest integer value.
 
+// By: Alaa Alokby
+// Sources: ASCII Table
+// Date: 25 November 2018
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
+
+#include <iostream>                                 // Standard input and output
+#include <fstream>                                  // File input and output
+#include <string>                                   // String type
+#include <cstdlib>                                  // exit()
 
 using namespace std;
 
 // Function prototypes
-void streamChar(int someArray[]);                   // Stream all characters from file into array
-int getShift(int someArray[]);                      // Determine character encryption shift
-string getFilePath();                               // Prompt user to enter path name, return path name
-void writeOut(string filePath, int shift);          // Create new decrypted file
+void streamChar(int someArray[], string encrypted);     // Stream all characters from file into array
+int getShift(int someArray[]);                          // Determine character encryption shift
+void getFilePath(string& input, string& output);        // Prompt user to enter path name, return path name
+void writeOut(string input, int shift, string output);  // Create new decrypted file
 
 int main(){
    
     // Declare variables
     int alphabetArray[25] = { 0 };                  // Create alphabet array with 25 elements set to 0
     int shift = 0;                                  // Declare and initialize variable to hold encryption shift
+    string efilePath;                               // Store file path for encrypted text
+    string dfilePath;                               // Store file path for deciphered text
+
+    getFilePath(efilePath, dfilePath);              // Prompt user for encrypted file path
    
     // Begin deciphering
-    streamChar(alphabetArray);                      // Stream from input file
+    streamChar(alphabetArray, efilePath);           // Stream from input file
     shift = getShift(alphabetArray);                // Get the encryption shift, store in 'shift'
-    writeOut("encrypted.txt", shift);               // Decipher, place deciphered text in file: 'd.txt'
+    writeOut(efilePath, shift, dfilePath);          // Decipher, place deciphered text in file: 'd.txt'
     
     return 0;
 }
+
 
 // streamChar
 // This function streams all characters from a file
@@ -44,144 +54,151 @@ int main(){
 // letters reside from alphabetArray[0] through alphabetArray[24]. The 
 // lowercase letters reside from alphabetArray[25] through alphabetArray[49].
 // Note that arrays are always passed by reference to functions.
-void streamChar(int array[]){
+void streamChar(int array[], string encrypted){
     // Declare variables
-    ifstream inputFile;             // Create input stream file object
-    char letter;                    // Create char object to store characters from input
-    int number;                     // Create int to store ASCII value of 'letter'
+    ifstream inputFile;                             // Create input stream file object
+    char letter;                                    // Create char object to store characters from input
+    int number;                                     // Create int to store ASCII value of 'letter'
     
-    // Open file
-    //inputFile.open(getFilePath());
-    inputFile.open("encrypted.txt");
-    if(!inputFile.is_open()){
+    inputFile.open(encrypted);                      // Open file using passed in file path
+    if(!inputFile.is_open()){                       // If input file is not open, exit program
         cout << "Could not open file at directed file path..." << endl;
         cout << "Exiting program." << endl;
-        exit(0);
+        exit(0);                                    // Exit and return value of 0
     }
     else{
-        while(inputFile.peek() != EOF){
-            inputFile >> noskipws >> letter;
-            
-            // Check if letter is lowercase or uppercase alphabet letter add 1 to corresponding element
+        while(inputFile.peek() != EOF){             // Peek at next line in file, True while not end of file
+            inputFile >> noskipws >> letter;        // Input stream from file, don't skip white space
             number = static_cast<int>(letter);
-            if(number >= 97 && number <= 122){                         // If letter is between a-z
-                array[number - 97] += 1;                               // Add one to corresponding position
+            
+            if(number >= 97 && number <= 122){      // If letter is in range a-z
+                array[number - 97] += 1;            // Increment corresponding element in array
             }           
         }        
-        inputFile.close();
+        inputFile.close();                          // Close the input file
     }
 }
 
+
 // getShift
-// This function finds the index of a passed-in array with the hightest
-// integer value.
+// This function finds the index of a passed-in array with the highest
+// integer value. Utilize the knowledge that ASCII value of 'e' is 101.
+// Use the index of the letter with the highest frequency to determine the
+// shift.
 int getShift(int array[]){
-    // Declare variable to store index of letter with highest frequency.
-    int index = 0;
-    int freq = 0;
-    int shift = 0;
-    int asciiValue = 0;
-    // Loop through array -- find the index and value of 'e'
-    for(int i = 0; i < 25; i++){
-        if(array[i] >= freq){
-            freq = array[i];
-            index = i;
-            cout << "Index: " << index << ", Value: " << freq << endl;
+
+    int index = 0;                                  // Used to store the index of the largest integer in array
+    int freq = 0;                                   // Used to store the value of the integer at index
+    int shift = 0;                                  // Store calculation for total shift
+    int asciiValue = 0;                             // Used to store the Ascii value of index
+
+    for(int i = 0; i < 25; i++){                    // Loop through array, find the index and value of 'e'
+        if(array[i] >= freq){                       
+            freq = array[i];                        // Assign value at index i to freq
+            index = i;                              // Assign index to i
         }
     }
-    // Use index of highest frequency letter to find shift
-    // ASCII - e: 101
-    asciiValue = index + 97;                                    // Get index in range of lower case letters
-    shift = -(101 - asciiValue);                                // Give shift implicit direction
-    cout << "Total Shift: " << shift << endl; 
-    return shift;                                               // Return shift
+    asciiValue = index + 97;                        // Get index in range of lower case letters
+    shift = -(101 - asciiValue);                    // Give shift implicit direction
+    return shift;                                   // Return shift
 }
+
 
 // getFilePath
 // This function prompts the user for the file path of the encrypted text.
 // The function returns the string of the file path.
-string getFilePath(){
-    string pathName;                                                            // Variable to store path name
-    cout << "Please enter the absolute path for the encrypted text file: " ;
-    getline(cin, pathName);
-
-    return pathName.c_str();                                                    // return c_str of pathName
+void getFilePath(string& input, string& output){
+    cout << "Please enter the absolute path for the encrypted text file: ";
+    getline(cin, input);
+    cout << "Please enter the absolute path for the clear-text file (deciphered output): ";
+    getline(cin, output);
 }
 
+
 // writeOut
-// This function reads encrypted input from fileName and creates a decrypted output file
-void writeOut(string fileName, int shift){
+// This function reads encrypted input from fileName and creates a deciphered output file
+void writeOut(string input, int shift, string output){
     
-    ifstream ifile;
-    ofstream ofile;
+    ifstream ifile;                                 // Input file
+    ofstream ofile;                                 // Output file
 
-    bool shifted = false;
-    int dnum;
-    char letter;
-    int anum;
-    int difference;
+    bool shifted = false;                           // Used to indicate if a character needs to be shifted
+    int dnum = 0;                                   // Holds ASCII value of deciphered number
+    char letter;                                    // Holds original char from file stream
+    int anum = 0;                                   // Hold original integer value of character
+    int difference = 0;                             // Used to make a correction to characters out of bounds
 
-    ifile.open(fileName);
-    ofile.open("d.txt");
+    ifile.open(input);
+    ofile.open(output);
+
+    if(!ifile.is_open()){                           // Handle unsuccessful attempt to open input file
+        cout << "Could not successfully open input file: " << input << endl;
+        cout << "Exiting program with value 0... " << endl;
+        exit(0);    
+    }
+    else{
+        cout << "Successfully opened input file: " << input << endl;
+    }
+    if(!ofile.is_open()){
+        cout << "Could not successfully open the output file: " << output << endl;
+        cout << "Exiting program with value 0... " << endl;
+        exit(0);
+    }
+    else{
+        cout << "Successfully opened output file: " << output << endl;
+    }
     while(ifile.peek() != EOF){
-        ifile >> noskipws >> letter;                    // get letter into char
-        anum = static_cast<int>(letter);                // num representation of letter
-        cout << "original letter: " << anum << " - " << letter << endl;
+        ifile >> noskipws >> letter;                // Stream character into char variable, include whitespaces
+        anum = static_cast<int>(letter);            // Determine the numerical representation of the character
         
         if(anum < 65 || anum > 90 && anum < 97 || anum > 122){                    
-            dnum = anum;                                // Case where decrypted number is same as original
-            cout << "OUT OF BOUNDS" << endl;
+            dnum = anum;                            // Case where there is no shift (punctuation, etc.)
         }
-        else if(anum >= 97 && anum <= 122){             // Handle lowercase letters
-            if(shift >= 0){                             // If the shift is greater than zero
-                dnum = anum - shift;                        // Attempt to shift numbers (decrypt) back
-                cout << "SHIFTED!!!" << endl;
-                shifted = true;                             // Set shifted bit to true
+        else if(anum >= 97 && anum <= 122){         // Handle lowercase letters
+            if(shift >= 0){                         // If the shift is greater than zero
+                dnum = anum - shift;                // Attempt to shift numbers (decrypt) back
+                shifted = true;                     // Set shifted bit to true
 
-                if(dnum < 97 && shifted == true){
-                    difference = 97 - dnum;
-                    dnum = 123 - difference;
+                if(dnum < 97 && shifted == true){   
+                    difference = 97 - dnum;         // Find how far out of bounds the shift pushed the char
+                    dnum = 123 - difference;        // Reset dnum to the correct character, roll around
                 }
             }
-            else if(shift <= 0){                        // If the shift is less than zero
-                dnum = anum - shift;                    // Shift forward (adding)
-                cout << "SHIFTED!!!" << endl;
-                shifted = true;
+            else if(shift <= 0){                    // If the shift is less than zero (backwards)
+                dnum = anum - shift;                // Shift forward to correct (adding)
+                shifted = true;                     // Set the shifted bit to true
 
                 if(dnum > 122 && shifted == true){
-                    difference = dnum - 122;
-                    dnum = 96 + difference;
+                    difference = dnum - 122;        // Find how far out of bounds the shift pushed the char
+                    dnum = 96 + difference;         // Reset dnum to the correct character, roll around
                 }
             }
         }
-        else if(anum >= 65 && anum <= 90){              // Handle uppercase letters
+        else if(anum >= 65 && anum <= 90){          // Handle uppercase letters
            if(shift >= 0){
-                dnum = anum - shift;
-                cout << "SHIFTED!!!" << endl;
-                shifted = true;
+                dnum = anum - shift;                // First attempt at correcting the Cipher shift
+                shifted = true;                     // Set shifted bit to true
 
                 if(dnum < 65 && shifted == true){         
-                    difference = 65 - dnum;
-                    dnum = 91 - difference;
+                    difference = 65 - dnum;         // Find how far out of bounds dnum is
+                    dnum = 91 - difference;         // Correct dnum
                 }
            }
            else if(shift <= 0){
-                dnum = anum - shift;
-                cout << "SHIFTED!!!" << endl;
-                shifted = true;
+                dnum = anum - shift;                // First attempt with correction shift
+                shifted = true;                     // Set shifted bit to true
 
-                if(dnum > 90 && shifted == true){
-                    difference = dnum - 90;
-                    dnum = 64 + difference;
+                if(dnum > 90 && shifted == true){   
+                    difference = dnum - 90;         // Find how far out of bounds initial correction was
+                    dnum = 64 + difference;         // Correct dnum
                 }
            }
         }
         
-        cout << "decrypted letter: " << dnum << " - " << static_cast<char>(dnum) << endl;
-        shifted = false;                                // Set shifted bit to false
-        ofile << static_cast<char>(dnum);
+        shifted = false;                            // Reset shifted bit to false
+        ofile << static_cast<char>(dnum);           // Stream out deciphered character
     }
-    ifile.close();
-    ofile.close();
+    ifile.close();                                  // Close input file
+    ofile.close();                                  // Close output file
 }
 
